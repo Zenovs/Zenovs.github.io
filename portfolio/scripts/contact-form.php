5c7139b40db5991558d3c648ece852751c9903be
Zenovs <dario@schnyder-werbung.ch>
2017-12-12 13:12:07 +0100

    asdf
    asdf

---
+++ portfolio/scripts/contact-form.php
@@ -0,0 +1,36 @@
+<?php
+$subjectPrefix = '[Kontaktaufnahme via Website]';
+$emailTo = '<info@kilian.so>';
+
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $name     = stripslashes(trim($_POST['form-name']));
+    $email    = stripslashes(trim($_POST['form-email']));
+    $betreff  = stripslashes(trim($_POST['form-betreff']));
+    $mitteilung = stripslashes(trim($_POST['form-mitteilung']));
+    $pattern  = '/[\r\n]|Content-Type:|Bcc:|Cc:/i';
+
+    if (preg_match($pattern, $name) || preg_match($pattern, $email) || preg_match($pattern, $betreff)) {
+        die("Header injection detected");
+    }
+
+    $emailIsValid = preg_match('/^[^0-9][A-z0-9._%+-]+([.][A-z0-9_]+)*[@][A-z0-9_]+([.][A-z0-9_]+)*[.][A-z]{2,4}$/', $email);
+
+    if($name && $email && $emailIsValid && $betreff && $mitteilung){
+        $subject = "$subjectPrefix $betreff";
+        $body = "Name: $name <br /> Email: $email <br /> Mitteilung: $mitteilung";
+
+        $headers  = 'MIME-Version: 1.1' . PHP_EOL;
+        $headers .= 'Content-type: text/html; charset=utf-8' . PHP_EOL;
+        $headers .= "From: $name <$email>" . PHP_EOL;
+        $headers .= "Return-Path: $emailTo" . PHP_EOL;
+        $headers .= "Reply-To: $email" . PHP_EOL;
+        $headers .= "X-Mailer: PHP/". phpversion() . PHP_EOL;
+
+        mail($emailTo, $subject, $body, $headers);
+        $emailSent = true;
+
+    } else {
+        $hasError = true;
+    }
+}
+?>
